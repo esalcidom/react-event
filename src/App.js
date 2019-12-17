858bc8f7e5605064b02c4e24ae31f531c073dbba
Emmanuel Salcido <salcidoe@amdocs.com>
2019-12-16 21:08:39 -0600

    Udemy follow project. Eventbrite api search event. Project done but non-functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListaEventos from './components/ListaEventos';
+
+import CategoriasProvider from './context/CategoriasContext';
+import EventosProvider from './context/EventosContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EventosProvider>
+      <CategoriasProvider>
+        <Header />
+        <div className="uk-container">
+          <Formulario/>
+          <ListaEventos/>
+        </div>
+      </CategoriasProvider>
+    </EventosProvider>
   );
 }
 
 export default App;
+
+//Notas de este proyecto
+// Se utilizara context api de react para poder enviar la informacion
+// de state o enviar funciones desde el componente principal hasta los hijos
+// Esto ayuda ya que no se necesita pasar el contexto entre cada componente
+// hasta llegar con el hijo. Los componenetes hijo tambien pueden actualizar
+// el state principal.
